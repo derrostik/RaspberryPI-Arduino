9439f24e3853edec59b19df10a6d802e7ec5f243
derrostik <46076241+derrostik@users.noreply.github.com>
2020-10-08 18:37:05 +0300

    Update two_arduino.py

--- two_arduino.py
+++ two_arduino.py
@@ -1,31 +1,13 @@
-import smbus
-import time
-import sys
-import serial
+from time import sleep
+import RPi.GPIO as gpio
 
-bus = smbus.SMBus(1)
-address1 = 0x04
-address2 = 0x05
-command = ""
+gpio.setmode(gpio.BOARD)
+gpio.setup(12, gpio.OUT)
+flag = 0
 
-while True:
-    try:
-        command = raw_input()
-        if(command == "1"):
-            bus.write_byte(address1, 1)
-            bus.write_byte(address2, 1)
-            print("1 - ON, 2 - ON")
-        elif(command == "2"):
-            bus.write_byte(address1, 1)
-            bus.write_byte(address2, 0)
-            print("1 - ON, 2 - OFF")
-        elif(command == "3"):
-            bus.write_byte(address1, 0)
-            bus.write_byte(address2, 1)
-            print("1 - OFF, 2 - ON")
-        else:
-            bus.write_byte(address1, 0)
-            bus.write_byte(address2, 0)
-            print("1 - OFF, 2 - OFF")
-    except ValueError:
-        print("Error")
+def open_roof():
+    gpio.output(12, flag)
+    sleep(1)
+    gpio.output(12, ~flag)
+    
+    gpio.cleanup()
