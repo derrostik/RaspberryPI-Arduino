136274b0a1864af0ad218e04eed27d8805567f97
derrostik <46076241+derrostik@users.noreply.github.com>
2020-11-07 17:31:41 +0300

    Update navigation.py

--- navigation.py
+++ navigation.py
@@ -1,15 +1,10 @@
 from time import sleep
 import RPi.GPIO as gpio
 
-pin = 13
-gpio.setmode(gpio.BOARD)
-gpio.setup(pin, gpio.OUT)
-flag = 1
-gpio.output(pin, ~flag)
-
 def navigation():
-    gpio.output(pin, flag)
+    pin = 13
+    gpio.setmode(gpio.BOARD)
+    gpio.setup(pin, gpio.OUT)
+    gpio.output(pin, 1)
     sleep(1)
-    gpio.output(pin, ~flag)
-    
-    gpio.cleanup()
+    gpio.output(pin, 0)
