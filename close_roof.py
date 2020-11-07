c6e2ec190fb504979c72f6aef5623c7ad9d18b17
derrostik <46076241+derrostik@users.noreply.github.com>
2020-11-07 17:32:24 +0300

    Update close_roof.py

--- close_roof.py
+++ close_roof.py
@@ -1,15 +1,10 @@
 from time import sleep
 import RPi.GPIO as gpio
 
-pin = 11
-gpio.setmode(gpio.BOARD)
-gpio.setup(pin, gpio.OUT)
-flag = 1
-gpio.output(pin, ~flag)
-
 def close_roof():
-    gpio.output(pin, flag)
+    pin = 11
+    gpio.setmode(gpio.BOARD)
+    gpio.setup(pin, gpio.OUT)
+    gpio.output(pin, 1)
     sleep(1)
-    gpio.output(pin, ~flag)
-    
-    gpio.cleanup()
+    gpio.output(pin, 0)
