491a839a22e03b950dad159ef6b93862f37fd40e
derrostik <46076241+derrostik@users.noreply.github.com>
2020-11-07 17:32:56 +0300

    Update open_roof.py

--- open_roof.py
+++ open_roof.py
@@ -1,15 +1,10 @@
 from time import sleep
 import RPi.GPIO as gpio
 
-pin = 12
-gpio.setmode(gpio.BOARD)
-gpio.setup(pin, gpio.OUT)
-flag = 1
-gpio.output(pin, ~flag)
-
 def open_roof():
-    gpio.output(pin, flag)
+    pin = 12
+    gpio.setmode(gpio.BOARD)
+    gpio.setup(pin, gpio.OUT)
+    gpio.output(pin, 1)
     sleep(1)
-    gpio.output(pin, ~flag)
-    
-    gpio.cleanup()
+    gpio.output(pin, 0)
